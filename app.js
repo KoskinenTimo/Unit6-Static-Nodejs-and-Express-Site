6da6761175147f7ef8109ee1df48bfb2e829977a
Timo Koskinen <koskinentht@gmail.com>
2021-03-20 23:56:57 +0200

    added texts and modified images, also error handling raw version done

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 const express = require('express');
 const app = express();
-const data = require('data.json').projects;
+const data = require('./data.json').projects;
+
+app.use('/static', express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
 app.set('view engine', 'pug');
-app.use('static', express.static('public'));
+
+app.get('/', (req,res) => {
+  res.render('index', {data});
+});
+
+app.get('/about', (req,res) => {
+  res.render('about');
+});
+
+app.get('/project/:id', (req,res) => {
+  const {id} = res.params;
+
+});
+
+app.use((req,res,next) => {
+  const error = new Error("Page Not Found.");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error,req,res,next) => {
+
+});
+
+app.listen(3000, () => {
+  console.log("Running on port 3000");
+});
